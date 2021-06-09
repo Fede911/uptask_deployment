138700f42edfbf7379c6a45efe232affc2d90024
Fede911 <federicobigatton@gmail.com>
2021-06-09 00:55:52 -0300

    Modificado el mensaje del servidor

--- index.js
+++ index.js
@@ -86,6 +86,6 @@ const host = process.env.HOST || '0.0.0.0';
 const port = process.env.PORT || 3000;
 
 app.listen(port, host, () => {
-    console.log('Servidor en puerto ', port);
+    console.log('Servidor en puerto: ', port);
 });
 
